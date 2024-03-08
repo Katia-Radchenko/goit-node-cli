01f6092491c30dfd5bdf50b0a6a66b806520a095
KaterynaRadchenko <82673991+Katia-Radchenko@users.noreply.github.com>
2024-03-08 15:41:05 +0200

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -2,7 +2,7 @@ import fs from "fs/promises";
 import path from "path";
 import {nanoid} from "nanoid"
 
-// const relPath = [];
+
 const contactsPath = path.resolve("db", "contacts.json");
 
 function updateContacts(contacts) {
